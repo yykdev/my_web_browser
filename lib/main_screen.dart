e280c32755a30d45adfada65a41213232f0b3ff8
YongYeon Kim <assavictory@gmail.com>
2023-12-21 00:28:09 +0900

    웹뷰 기능 구현

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -8,11 +11,49 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  late WebViewController _webViewController;
+
+  @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) WebView.platform = AndroidWebView();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('나만의 웹브라우저'),
+        title: const Text('나만의 웹브라우저'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.add),
+          ),
+          PopupMenuButton<String>(
+              onSelected: (value) {
+                _webViewController.loadUrl(value);
+              },
+              itemBuilder: (context) => [
+                    const PopupMenuItem<String>(
+                      value: 'https://www.google.com',
+                      child: Text('구글'),
+                    ),
+                    const PopupMenuItem<String>(
+                      value: 'https://www.naver.com',
+                      child: Text('네이버'),
+                    ),
+                    const PopupMenuItem<String>(
+                      value: 'https://www.kakao.com',
+                      child: Text('카카오'),
+                    ),
+                  ]),
+        ],
+      ),
+      body: WebView(
+        initialUrl: 'https://flutter.dev',
+        onWebViewCreated: (controller) {
+          _webViewController = controller;
+        },
       ),
     );
   }
